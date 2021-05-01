52419381bc0424c6c2d4d4c25ec27fd9dac9c49b
JasonZhangggg <jasonpogchamp@gmail.com>
2021-04-30 20:37:28 -0700

    updated redSide2 to handle 4-ring automode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redSide2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redSide2.java
@@ -36,76 +36,128 @@ public class redSide2 extends LinearOpMode {
         telemetry.addData("ringNum", ringNum);
         telemetry.update();
 
-        Trajectory traj_to_shoot_pos = drive.trajectoryBuilder(new Pose2d())
-                .strafeTo(new Vector2d(50, 0))
+        Trajectory traj_to_ringstack0 = drive.trajectoryBuilder(new Pose2d())
+                .strafeTo(new Vector2d(25, -6))
                 .build();
-        Trajectory traj_back_from_ringstack = drive.trajectoryBuilder(traj_to_shoot_pos.end())
-                .back(5)
+        Trajectory traj_to_shoot_pos = drive.trajectoryBuilder(traj_to_ringstack0.end())
+                .forward(25)
                 .build();
 
-        Trajectory traj_to_ringstack1 = drive.trajectoryBuilder(traj_back_from_ringstack.end())
-                .forward(3)
+        Trajectory traj_to_ringstack1 = drive.trajectoryBuilder(traj_to_shoot_pos.end())
+                .forward(2)
                 .build();
         Trajectory traj_to_ringstack2 = drive.trajectoryBuilder(traj_to_ringstack1.end())
-                .forward(3)
+                .forward(2)
                 .build();
         Trajectory traj_to_ringstack3 = drive.trajectoryBuilder(traj_to_ringstack2.end())
                 .forward(3)
                 .build();
+      //  Trajectory traj_back_from_ringstack = drive.trajectoryBuilder(traj_to_ringstack3.end())
+      //          .back(0.1)
+      //          .build();
+
+        // wobble goals
+        Trajectory traj_drop_1st_wg = drive.trajectoryBuilder(traj_to_ringstack3.end())
+                .strafeTo(new Vector2d(100, -25))
+                .build();
+        Trajectory traj_drop_1st_wg_back = drive.trajectoryBuilder(traj_drop_1st_wg.end())
+                .back(5)
+                .build();
+        Trajectory traj_pick_2nd_wg = drive.trajectoryBuilder(traj_drop_1st_wg_back.end())
+                .lineToLinearHeading(new Pose2d(28, -23, Math.toRadians(180)))
+                .build();
+
+        Trajectory traj_pick_2nd_wg_fwd = drive.trajectoryBuilder(traj_pick_2nd_wg.end())
+                .forward(8)
+                .build();
+        Trajectory traj_drop_2nd_wg = drive.trajectoryBuilder(traj_pick_2nd_wg_fwd.end())
+                .lineToLinearHeading(new Pose2d(100, -18, 0))
+                .build();
+
+        Trajectory traj_drop_2nd_wg_back = drive.trajectoryBuilder(traj_drop_2nd_wg.end())
+                .back(25)
+                .build();
+        Trajectory traj_end = drive.trajectoryBuilder(traj_drop_2nd_wg_back.end())
+                .lineToLinearHeading(new Pose2d(75, 5, 0))
+                .build();
 
         //___________________________________________________________________________________________
         arm.rotateBackward(false);
-
-        //drive to shoot position w/ intake on reversely
-        intake.setReverse();
-        intake.turnOnSlow();
-        drive.followTrajectory(traj_to_shoot_pos);
-        intake.unsetReverse();
-        intake.turnOff();
-
-        shooter.setPower(1850);
-        shooter.turnOn();
         shooter.shootPos();
+        intake.deploy();
+
+        //drive to shoot position
+        drive.followTrajectory(traj_to_ringstack0);
+        shooter.turnOn();
+        drive.followTrajectory(traj_to_shoot_pos);
 
         // first 3 rings
-        sleep(1750);
+        drive.turn(Math.toRadians(9));
+        // sleep(1000);
         shoot();
         shoot();
         shoot();
         shooter.turnOff();
-
-        drive.followTrajectory(traj_back_from_ringstack);
-
+        drive.turn(Math.toRadians(-9));
 
         // intake rings
-        intake.deploy();
         shooter.storePos();
         intake.turnOn();
         drive.followTrajectory(traj_to_ringstack1);
-        sleep(2000);
+        //sleep(500);
         drive.followTrajectory(traj_to_ringstack2);
-        sleep(2000);
+        //sleep(1000);
         drive.followTrajectory(traj_to_ringstack3);
-        sleep(2000);
+        sleep(1500);
+        intake.turnOff();
 
+        // shoot (2nd)
+        shooter.shootPos();
+        sleep(300);
+        shooter.setPower(2200);
+        shooter.turnOn();
+        // drive.followTrajectory(traj_back_from_ringstack);
+
+        // first 3 rings
+        drive.turn(Math.toRadians(9));
+        //sleep(1000);
+        shoot();
+        shoot();
+        shoot();
+        shooter.turnOff();
+        drive.turn(Math.toRadians(-9));
+
+        // drop 1st whole goal
+        drive.followTrajectory(traj_drop_1st_wg);
+        drop();
+        drive.followTrajectory(traj_drop_1st_wg_back);
+
+        drive.followTrajectory(traj_pick_2nd_wg);
+        drive.followTrajectory(traj_pick_2nd_wg_fwd);
+        pick();
+        drive.followTrajectory(traj_drop_2nd_wg);
+        drop();
+        drive.followTrajectory(traj_drop_2nd_wg_back);
+
+        //drive.followTrajectory(traj_end);
     }
 
     public void shoot() {
         shooter.trigger();
-        sleep(200);
+        sleep(130);
         shooter.neutral();
-        sleep(200);
+        sleep(130);
     }
 
     public void drop() {
         arm.rotateForward();
-        sleep(500);
+        //sleep(10);
         arm.open();
-        sleep(250);
+        //sleep(50);
     }
     public void pick(){
         arm.close();
-        sleep(500);
+        sleep(100);
         arm.rotateBackward(false);
     }
 }
